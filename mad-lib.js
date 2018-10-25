a67559ff221f2c2696367c1e16a4f32e3c8695d1
cmacfadyen <10223598+cmacfadyen@users.noreply.github.com>
2018-10-25 08:52:16 -0400

    Create mad-lib.js

---
+++ mad-lib.js
@@ -0,0 +1,9 @@
+var animal = window.prompt("Enter a noun");
+var action = window.prompt(Enter a verb");
+var adj = window.prompt("Enter an adjective");
+
+var story1 = "Yesterday I got a new pet.  It's a ";
+var story2 = "! It's kind of ";
+var story3 = "and it likes to ";
+
+document.getElementById("story").innerHTML = story1 + animal + story2 + adj + story 3 + action;
